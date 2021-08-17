57dbb5a82388a009b16da707d5daed2e3f3cd301
DanilHas <hasanovdan@gmail.com>
2021-08-17 15:38:18 +0300

    create new todo list

--- src/index.js
+++ src/index.js
@@ -1,148 +1,193 @@
-let tasksStore = [];
+class Task {
+  constructor(title) {
+    this.id = Math.random().toString(36).slice(2);
+    this.title = title;
+    this.completed = false;
+  }
+}
 
-const searchField = document.createElement('input');
-searchField.className = "todo-list__search-field";
-searchField.placeholder = "Add Task";
-
-const searchButton = document.createElement('button');
-searchButton.className = "todo-list__button";
-searchButton.textContent = "Add";
-
-const searchForm = document.createElement('form');
-searchForm.className = "todo-list__form";
-
-const searchContainer = document.createElement('div');
-searchContainer.className = "todo-list__top-container";
-
-const tasksContainer = document.createElement('div');
-tasksContainer.className = "todo-list__footer-container";
+let tasksStore = [
+  // {
+  //   id: 'some id',
+  //   title: 'here is some title',
+  //   completed: false,
+  // },
+  new Task('some task 2'),
+];
 
 const mainContainer = document.createElement('main');
-mainContainer.className = "todo-list";
+mainContainer.className = 'todo-list';
 
-searchForm.append(searchField, searchButton);
-searchContainer.append(searchForm);
-mainContainer.append(searchContainer, tasksContainer);
+const tasksContainer = document.createElement('div');
+tasksContainer.className = 'todo-list__tasks-container';
+
+const title = document.createElement('h1');
+title.className = 'todo-list__title';
+title.textContent = 'To Do List';
+
+const addButton = document.createElement('button');
+addButton.className = 'todo-list__add-button';
+
+const imagePlus = document.createElement('img');
+imagePlus.className = 'todo-list__image-plus';
+imagePlus.src = './assets/icons/Plus.svg';
+
+const topContainer = document.createElement('div');
+topContainer.className = 'todo-list__top-container';
+
+addButton.append(imagePlus);
+topContainer.append(title, addButton);
+mainContainer.append(topContainer, tasksContainer);
 document.body.append(mainContainer);
 
-class Task {
-constructor(title, completed = false) {
-this.id = Math.random().toString(36).slice(2);
-this.completed = completed;
-this.title = title;
-}
-}
+addButton.onclick = function () {
+  // Мы должны создать таск контейнер для того, чтобы
+  // хранить в нем дургие элементы (инпут, чекбокс, текст, кнопки)
+  const task = document.createElement('div');
+  // Вешаем на таск контейнер классы - один обычный для стилей - "todo-list__task"
+  // и второй класс для того, чтобы понять что данный контейнер
+  // у нас находится в работе (т.е. мы его создаем) - "todo-list__task-creating"
+  task.className = 'todo-list__task todo-list__task-creating-now';
 
+  // Создаем контейнер, который нам нужен для того, чтобы хранить в нем чекбокс, инпут/текст и дату
+  const taskLeftContainer = document.createElement('div');
+  taskLeftContainer.className = 'todo-list__task-left-container';
+  // Далее мы должны создать чекбокс, который мы засунем в таск контейнер
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  checkBox.className = 'todo-list__checkbox';
 
-function renderTasks() {
-tasksContainer.innerHTML = '';
-tasksStore.forEach((task) => {
-const unfinishedTask = document.createElement('div');
-unfinishedTask.id = task.id;
-unfinishedTask.className = 'todo-list__task';
-unfinishedTask.textContent = task.title;
+  // Далее мы создаем инпут в котором мы будем писать название таски
+  const tasksInput = document.createElement('input');
+  tasksInput.className = 'todo-list__input';
 
-if (task.completed) {
-unfinishedTask.classList.add('todo-list__item-completed');
-}
+  // TODO:
+  // Нужно переделать так, чтобы при нажатии кнопки enter в инпуте
+  // у нас создавалась таска
+  tasksInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      const taskContainer = document.querySelector('.todo-list__task-creating-now');
+      const input = taskContainer.querySelector('.todo-list__input');
 
-const deleteButton = document.createElement('button');
-deleteButton.className = "todo-list__deleteButton";
-deleteButton.textContent = "X";
-deleteButton.onclick = function () {
-unfinishedTask.remove();
-tasksStore = tasksStore.filter((task) => task.id !== unfinishedTask.id);
-saveInLocalStorage();
+      if (input && !input.value) {
+        taskContainer.remove();
+        return;
+      } else {
+        taskContainer.remove();
+        tasksStore.push(new Task(input.value));
+        renderTasks();
+      }
+    }
+  });
+
+  // Добавляем элементы в контейнер с эелментами слева
+  taskLeftContainer.append(checkBox, tasksInput);
+
+  task.append(taskLeftContainer);
+
+  // Добавляем контейнер с элементами слева в контейнер
+  tasksContainer.prepend(task);
+
+  // Вызываем метод фокус, чтобы курсор появился в инпуте, который мы только что добавили
+  tasksInput.focus();
 };
 
-const taskCheckBox = document.createElement('input');
-taskCheckBox.setAttribute ('type', 'checkbox');
-taskCheckBox.className = 'todo-list__checkbox';
+document.addEventListener('click', function (event) {
+  const taskContainer = document.querySelector('.todo-list__task-creating-now');
 
-const editButton = createEditButton();
-const edit = editTasks();
+  if (event.target.className === 'todo-list__add-button' || taskContainer.contains(event.target)) {
+    return;
+  }
 
-unfinishedTask.append(editButton);
-tasksContainer.prepend(unfinishedTask);
-unfinishedTask.append(deleteButton);
-unfinishedTask.prepend(taskCheckBox);
-unfinishedTask.append(editButton);
-unfinishedTask.append(edit);
+  const input = taskContainer.querySelector('.todo-list__input');
 
-searchField.value = '';
+  if (input && !input.value) {
+    taskContainer.remove();
+    return;
+  } else {
+    taskContainer.remove();
+    tasksStore.push(new Task(input.value));
+    renderTasks();
+  }
 });
-saveInLocalStorage();
-}
 
-function createEditButton() {
+function renderTasks() {
+  const tasksContainer = document.querySelector('.todo-list__tasks-container');
+  tasksContainer.innerHTML = '';
+
+  tasksStore.forEach((task) => {
+    // создаем конейнер для таски
+    const taskContainer = document.createElement('div');
+    taskContainer.className = 'todo-list__task';
+    taskContainer.id = task.id;
+
+    // Создаем контейнер, который нам нужен для того, чтобы хранить в нем чекбокс, текст и дату
+    const taskLeftContainer = document.createElement('div');
+    taskLeftContainer.className = 'todo-list__task-left-container';
+    // Далее мы должны создать чекбокс, который мы засунем в таск контейнер
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.className = 'todo-list__checkbox';
+
+    // Создаем параграф для того, чтобы добавить в него
+    // текст который мы только что написали в инпуте
+    const taskTitle = document.createElement('p');
+    taskTitle.className = 'todo-list__task-title';
+    taskTitle.textContent = task.title;
+
+    // Создаем элемент который будет хранить дату создания/обновления нашей таски
+    const taskUpdatingDate = document.createElement('p');
+    taskUpdatingDate.className = 'todo-list__task-updating-date';
+    taskUpdatingDate.textContent = new Date().toDateString();
+
+    const taskRightContainer = document.createElement('div');
+    taskRightContainer.className = 'todo-list__task-right-container';
+
     const editButton = document.createElement('button');
-editButton.className = "todo-list__editButton";
+    editButton.className = 'todo-list__editButton';
 
-const imagePencil = document.createElement('img');
-imagePencil.className = 'todo-list__image' 
-imagePencil.src = 'C:/Users/Danil/Desktop/pencil.jpg';
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'todo-list__deleteButton';
 
-editButton.append(imagePencil);
-return editButton;
-}
+    deleteButton.onclick = (event) => {
+      const taskContainer = event.target.closest('.todo-list__task');
+      tasksStore = tasksStore.filter((task) => task.id !== taskContainer.id);
+      renderTasks();
+    };
 
-function editTasks() {
-    const editButton = createEditButton();
+    const imageDeleteButton = document.createElement('img');
+    imageDeleteButton.className = 'todo-list__imageDeleteButton';
+    imageDeleteButton.src = '../js_vanila_todo_list/assets/icons/DeleteIcon.svg';
 
-    const input = document.createElement('input');
-    input.className = 'todo-list__editTaskInput';
+    const imageEditButton = document.createElement('img');
+    imageEditButton.className = 'todo-list__imageEditButton';
+    imageEditButton.src = '../js_vanila_todo_list/assets/icons/EditIcon.svg';
 
-    const okButton = document.createElement('button');
-    okButton.className = 'todolist__editOkButton';
+    editButton.append(imageEditButton);
+    deleteButton.append(imageDeleteButton);
+    taskRightContainer.append(editButton, deleteButton);
+    taskLeftContainer.append(taskTitle, taskUpdatingDate);
+    taskContainer.append(checkBox, taskLeftContainer, taskRightContainer);
 
-    const cancelButton = document.createElement('button');
-    cancelButton.className = 'todo-list__cancelButton';
-
-    editButton.onclick = function() {
-        input.value = searchField.value;
-        if (okButton.onclick) {
-            input.value = task.title;
-        } else if (cancelButton.onclick) {
-            input.value = searchField.value;
-        }
-    }
-    input.append(okButton);
-    input.append(cancelButton);
-}
-
-function completeTask(event) {
-const checkbox = event.target;
-const taskContainer = checkbox.closest('div');
-const taskIdx = tasksStore.findIndex((task) => task.id == taskContainer.id);
-
-if (taskIdx !== -1) {
-tasksStore[taskIdx].completed = true;
-}
-
-renderTasks();
+    tasksContainer.prepend(taskContainer);
+  });
 }
 
 function saveInLocalStorage() {
-localStorage.setItem('tasksStore', JSON.stringify(tasksStore));
+  localStorage.setItem('tasksStore', JSON.stringify(tasksStore));
 }
 
 function readFromLocalStorage() {
-tasksStore = JSON.parse(localStorage.getItem('tasksStore')) || [];
+  tasksStore = JSON.parse(localStorage.getItem('tasksStore')) || [];
+  console.log(tasksStore);
 }
-
-function createNewTask (event) {
-console.log('event', event);
-event.preventDefault();
-if (searchField.value) {
-// Создали таск и добавили его в хранилище
-const task = new Task(searchField.value);
-tasksStore.push(task);
 
 renderTasks();
-}
-}
 
-searchForm.addEventListener('submit', createNewTask);
-
-readFromLocalStorage();
-renderTasks();
+// 1. Создать правый контейнер
+// 2. Создать две кнопки и засунуть в них картинки с иконками (и добавить стили)
+// 3. поместить эти кнопки в правый контейнер
+// 4. Добавить правый конейнер в taskContainer
+// 5. Сделать стили так, чтобы соответствовало дизайнам
+// 6. Реализовать функционал по удалению таски
+// 7. Реализовать функционал по редактированию таски (хард левел)
